42a6a436bdb93fc5473e6a8a4dcd2fe9e6bfdf38
Daniel Klabbers <daniel@productip.com>
2017-11-15 16:55:22 +0100

    wip

--- config/discord.php
+++ config/discord.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-    'bot-token' => env('DISCORD_BOT_TOKEN')
+    /**
+     * Set up an application and mark it as Bot.
+     *
+     * @see https://discordapp.com/developers/applications/me
+     */
+    'bot-token' => env('DISCORD_BOT_TOKEN'),
+    'endpoints' => [
+        'gateway' => 'wss://gateway.discord.gg',
+        'http-api' => 'https://discordapp.com/api/'
+    ],
+    'versions' => [
+        'gateway' => 6,
+        'http-api' => 6
+    ]
 ];
---
+++ config/extensions.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    /**
+     * The list of extensions to enable by default.
+     *
+     * @info Use the exact extension name; discodian/ext-patreon
+     */
+    'enabled' => []
+];
