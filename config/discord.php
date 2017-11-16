278ff2755c2a1d52d92b5169ae329dc5f9834f31
luceos <daniel+github@klabbers.email>
2017-11-16 01:05:08 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/discord.php
+++ config/discord.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the Discodian bot toolkit.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://discodian.com
+ * @see https://github.com/discodian
+ */
+
 return [
     /**
      * Set up an application and mark it as Bot.
